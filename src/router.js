8d5a423ad31c66c730ffe52cdbf86bd8083cb2b8
Artem <selfex9496@gmail.com>
2024-09-06 13:14:50 +0700

    add routing, install sass

---
+++ src/router.js
@@ -0,0 +1,14 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+import Home from './components/Home'
+
+Vue.use(Router)
+
+const routes = [{ path: '/', component: Home }]
+
+const router = new Router({
+  mode: 'history',
+  routes,
+})
+
+export default router
